e4c5e94caeb3895fba1188fbe68c6eda818e1160
KWagner91 <kerstin-wagner@gmx.at>
2018-05-07 19:20:51 +0200

    feat: player class and functions

--- js/app.js
+++ js/app.js
@@ -30,13 +30,37 @@ var Player = function(x, y){
     this.sprite = 'images/char-boy.png';
 };
 
-var player = new Player(200,400);
+
+player.update = function(dt) {
+};
+
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+player.handleInput = function(direction){
+
+          if(direction === 'left' && this.x !== 0){
+     this.x -= 100;
+     }
+     if(direction === 'up' && this.y > 0){
+     this.y -= 82.5;
+     }
+     if(direction === 'right' && this.x < 400){
+     this.x += 100;
+     }
+     if(direction === 'down' && this.y < 400){
+     this.y += 82.5;
+     }
+    };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var player = new Player(200,400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
