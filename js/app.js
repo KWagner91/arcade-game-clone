76ab06ff10ee809f150003ef4b5f85534f72c92a
KWagner91 <kerstin-wagner@gmx.at>
2018-05-07 19:22:01 +0200

    feat: player moving function

--- js/app.js
+++ js/app.js
@@ -62,6 +62,30 @@ player.handleInput = function(direction){
 
 var player = new Player(200,400);
 
+player.update = function(dt) {
+};
+
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+player.handleInput = function(direction){
+
+          if(direction === 'left' && this.x !== 0){
+     this.x -= 100;
+     }
+     if(direction === 'up' && this.y > 0){
+     this.y -= 82.5;
+     }
+     if(direction === 'right' && this.x < 400){
+     this.x += 100;
+     }
+     if(direction === 'down' && this.y < 400){
+     this.y += 82.5;
+     }
+    };
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
